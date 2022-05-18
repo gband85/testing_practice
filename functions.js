77c29949d3df219efed2d17b3c044c80386c2ed0
gband85 <garrett@gband.net>
2022-05-18 14:45:03 -0500

    fix algorithm

--- functions.js
+++ functions.js
@@ -23,7 +23,7 @@ return {add,subtract,multiply,divide}
 }
 //convert to array
 function strToArr(string){
-return string.split("")
+return Array.from(string)
 }
 //check if char is punctuation
 function isPunctuation(char){
@@ -42,6 +42,26 @@ return char.charCodeAt(0)
 }
 function joinArr(arr){
     return arr.join("")
+}
+
+function caesarCipher(string){
+let strArr=strToArr(string)
+let codeArr=strArr.map(x=>x.charCodeAt(0))
+let shiftedArr=codeArr.map(function(x){
+    if (x>=65 && x<=90) {
+if (x>77) {
+    return x-13;
+}
+else  {
+    return x+13;
+}
+    }
+    else {
+        return x;
+    }
+})
+let newArr= shiftedArr.map(String.fromCharCode(x))
+return joinArr(newArr)
 }
 
 export { capitalize, reverseString,calculator,isPunctuation}
